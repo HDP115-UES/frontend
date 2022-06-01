7d837a8834eb1936efccbdbf1dfa2d16ac5b13e9
Kevin Grande <kevingrande2002@gmail.com>
2022-06-01 17:06:12 -0600

    Routes and Navbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { NavbarPage } from "./Components/Navbar/Navbar";
+import { GetAccidents, HomePage, ReportAccident, SignUp } from "./Pages/index";
+import { Login } from "./Pages/Login/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavbarPage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/getAccidents" element={<GetAccidents />} />
+          <Route path="/addReport" element={<ReportAccident />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
