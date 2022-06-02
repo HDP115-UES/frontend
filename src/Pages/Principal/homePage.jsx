fba25b063d9c86583ae6d0c83ee3fe4f972a534a
Kevin Grande <kevingrande2002@gmail.com>
2022-06-01 18:55:18 -0600

    Home UI added

--- src/Pages/Principal/homePage.jsx
+++ src/Pages/Principal/homePage.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./homePage.css";
+import reportAccident from "../../Assets/Images/reportAccident.svg";
+import getAccident from "../../Assets/Images/get-reports.svg";
 export const HomePage = () => {
   return (
-    <div>Reporta un accidente</div>
-  )
-}
+    <Container className="seccionPrincipal" fluid>
+      <Container className="textContainer d-flex align-items-center w-100 h-100">
+        <h1>Reporta un accidente</h1>
+      </Container>
+      <Container className="mt-5">
+        <Row>
+          <Col xs={12} sm={6}>
+            <Row className="containerReport ">
+              <Col className="d-flex align-items-center justify-content-center">
+                <img src={reportAccident}></img>{" "}
+                <span className="ms-4">Reportar un accidente</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={12} sm={6}>
+            <Row className="containerReport">
+              <Col className="d-flex align-items-center justify-content-center">
+                <img src={getAccident}></img>{" "}
+                <span className="ms-4">Reportar un accidente</span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
