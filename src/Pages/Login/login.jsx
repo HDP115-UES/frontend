85bc8b90fd5c75168ec9a61087e27a47d30a9d1c
Kevin Grande <kevingrande2002@gmail.com>
2022-06-14 22:35:50 -0600

    removed unused componentes

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container,Form, Button } from "react-bootstrap";
 import "./login.css";
 import arrowBack from "../../Assets/Images/arrow-back.svg";
 
